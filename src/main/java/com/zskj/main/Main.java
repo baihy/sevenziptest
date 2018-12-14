8736069789578e2af5f9e4b4e086128706f1e5c1
baihuayang <1053091115@qq.com>
2018-12-14 17:07:19 +0800

    解决了主类冲突

--- src/main/java/com/zskj/main/Main.java
+++ src/main/java/com/zskj/main/Main.java
@@ -15,6 +15,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Integer numberOfItemsInArchive = CompressUtils.getNumberOfItemsInArchive("/Users/baihuayang/Downloads/abc.zip");
         System.out.println(numberOfItemsInArchive);
-    }
+}
 
 }
