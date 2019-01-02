8fca3d54dfe901cfba59763738dbd764d48d1273
huayang.bai <1053091115@qq.com>
2019-01-02 14:56:37 +0800

    修改了main方法

--- src/main/java/com/zskj/main/Main.java
+++ src/main/java/com/zskj/main/Main.java
@@ -1,8 +1,5 @@
 package com.zskj.main;
 
-import java.io.File;
-import java.io.IOException;
-
 /**
  * @ProjectName: sevenziptest
  * @packageName: com.zskj.main
@@ -13,17 +10,9 @@ import java.io.IOException;
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
-        String str = "/Users/baihuayang/Downloads/aaa/abc";
-        File file = new File(str);
-        File parentFile = file.getParentFile();
-        if (!parentFile.exists()) {
-            parentFile.mkdirs();
-        }
-        boolean newFile = file.createNewFile();
-        System.out.println(newFile);
-        System.out.println("判断是否是文件：" + file.isFile());
-        System.out.println("判断是否是文件夹：" + file.isDirectory());
+    public static void main(String[] args) {
+
+        System.out.println("***********main方法*************");
 
     }
 
